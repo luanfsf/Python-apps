3779f23c7c93c6330b875b7d7558693cfa57e536
luanfsf <luanfsf@gmail.com>
2017-04-20 04:16:31 -0300

    read

--- TODO.py
+++ TODO.py
@@ -1,24 +1,48 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 def main():
 
 def readDB():
+	''' Ler o arquivo e para cada linha, ler tarefa e prioridade
+	'''
+
+	todolist = []
+
+	with open('todos.db') as db:
+	   for line in db:
+		  todolist.append(line.strip().split(','))
+		  
+	'''
+	# Para ver itens por linha
+	for i in range(len(display_list)):
+		for j in range(len(display_list[i])):
+			print(display_list[i][j] , end=",") # [i][j]
+		print("")
+	
+	#print(len(display_list[0])) # Para ver numero de linhas (i)
+	#print(len(display_list[0])) # Para ver numero de itens em cada linha (j)
+	'''
+	    
 def saveDB():
 
-def formatTasks():
+class task ():
+	# MAXIMUM TASKS 999
+	# PRIORITY (RED,GREEN, YELLOW)
+	# NUM-PRIORITY-111111111111111111111111111111111111111111111111111111111111
+	# max 70 characteres
 
-def endPrompt(): # end of line menu
+	def formatTasks():
 
-def addTask():
-def editTask():
-def removeTask():
+	def endPrompt(): # end of line menu
 
-#MAXIMUM TASKS 999
-#PRIORITY (RED,GREEN, YELLOW)
-#NUM-PRIORITY-111111111111111111111111111111111111111111111111111111111111
-#max 70 characteres
-
+	def addTask():
+	
+	def editTask():
+	
+	def removeTask():
+	
+	def sortpriority():
 
 if __name__ == '__main__':
 	main()

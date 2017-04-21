799b65c58583d925de2a12905736e245bd9f8dbd
luanfsf <luanfsf@gmail.com>
2017-04-21 02:47:30 -0300

    pseudocodigo

--- TODO.py
+++ TODO.py
@@ -2,29 +2,63 @@
 # encoding: utf-8
 
 def main():
+	'''
+	Pseudocodigo
+
+	iniciar e ler a lista principal
+	exibir status 0 a 100
+	exibir a lista principal
+	exibir menu de acoes
+		newtask
+		editTask
+		removeTask
+
+
+	'''
 
 def readDB():
 	''' Ler o arquivo e para cada linha, ler tarefa e prioridade
 	'''
 
-	todolist = []
+todolist = []
 
-	with open('todos.db') as db:
-	   for line in db:
-		  todolist.append(line.strip().split(','))
-		  
+with open('todos.db') as db:
+	for line in db:
+	   todolist.append(line.strip().split(';'))
+
 	'''
 	# Para ver itens por linha
 	for i in range(len(display_list)):
 		for j in range(len(display_list[i])):
 			print(display_list[i][j] , end=",") # [i][j]
 		print("")
-	
+
 	#print(len(display_list[0])) # Para ver numero de linhas (i)
 	#print(len(display_list[0])) # Para ver numero de itens em cada linha (j)
+
+
 	'''
-	    
+
 def saveDB():
+
+def formatTasks():
+	'''
+	UNIX
+	┘ ┐ ┌ └ ┼ ─ ├ ┤ ┴ ┬ │
+
+	DOS
+	│	┤					    ╣║╗╝	┐
+	└	┴	┬	├	─	┼		╚╔╩╦╠═╬
+	┘	┌	┐
+	'''
+
+def endPrompt(): # end of line menu
+
+def header():
+
+def sortpriority():
+
+def addTask():
 
 class task ():
 	# MAXIMUM TASKS 999
@@ -32,17 +66,10 @@ class task ():
 	# NUM-PRIORITY-111111111111111111111111111111111111111111111111111111111111
 	# max 70 characteres
 
-	def formatTasks():
-
-	def endPrompt(): # end of line menu
-
-	def addTask():
-	
 	def editTask():
-	
+
 	def removeTask():
-	
-	def sortpriority():
+
 
 if __name__ == '__main__':
 	main()
